e0b2702a1b7b3bbd8b2595361ac7a63bad81e71a
Sid <SidBayeck@outlook.com>
2022-11-23 09:00:51 +0800

    Add haskell support

---
+++ modules/lang/haskell.el
@@ -0,0 +1,17 @@
+;;; HASKELL --- Haskell Support -*- lexicalbinding: t; -*-
+;;
+;; Author Sid <SidBayeck@outlook.com>
+;; Copyright @ 2022, Sid, all rights reserved.
+;; Created: 24 九月 2022
+;;
+;;; Commentary:
+;;
+;; >>= Haskell
+;;
+;;; Code:
+
+(elpaca-use-package haskell-mode)
+
+(provide 'lang/haskell)
+
+;;; haskell.el ends here
